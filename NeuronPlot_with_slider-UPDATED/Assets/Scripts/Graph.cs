bfffcc09dfe6760e17ae409899d26f68ac19a3ea
rohit-masur <masur.rohit@gmail.com>
2018-04-11 17:10:19 -0400

    added visuals

--- NeuronPlot_with_slider-UPDATED/Assets/Scripts/Graph.cs
+++ NeuronPlot_with_slider-UPDATED/Assets/Scripts/Graph.cs
@@ -15,9 +15,21 @@ public class Graph : MonoBehaviour {
 	public Text dataEndText; 
 	public Text dataSpikesText;
 	public Text dataTimeInSeconds;
+	public Text dataTimeInSecondsLow;
+	public Text dataTimeInSecondsMedium;
+	public Text dataTimeInSecondsHigh;
+	public Text dataLow;
+	public Text dataHigh;
+	public Text dataMedium;
+	public int maxRange;
 
-	private int maxRange =10;
+
 	private int noOfSpikes=0;
+	private int lowSpikes = 0;
+	private int mediumSpikes = 0;
+	private int highSpikes = 0;
+
+
 	private float xval;
 	private float yval;
 	private float nspk;
@@ -36,21 +48,42 @@ public class Graph : MonoBehaviour {
 	private float updivvalue = 10000;
     private List<Dictionary<string, object>> data;
 	private ArrayList timeOfSpike = new ArrayList();
+	private ArrayList timeOfSpikeLow = new ArrayList();
+	private ArrayList timeOfSpikeMedium = new ArrayList();
+	private ArrayList timeOfSpikeHigh = new ArrayList();
 
-	void showRange(int range, int spikes, string times){
+
+
+	void showRange(int range, int spikes, int low, int high, int medium, string timesLow, string timesMedium, string timesHigh){
 		
 		int tillRange = range + maxRange -1;
 		string startingPoint = range.ToString ();
 		string end = tillRange.ToString ();
 		dataStartText.text = "Datapoints: "+ startingPoint;
 		dataEndText.text = " to " + end;
-		dataSpikesText.text = "Total Spikes: "+ spikes.ToString();
+		dataSpikesText.text = "Total Spikes: "+ (low+medium+high).ToString();
 
-		if (!(string.IsNullOrEmpty (times))) {
-			dataTimeInSeconds.text = "Time of spikes: " + times;
+		if (!(string.IsNullOrEmpty (timesLow))) {
+			dataTimeInSecondsLow.text =  timesLow;
 		} else {
-			dataTimeInSeconds.text = string.Empty;
+			dataTimeInSecondsLow.text = string.Empty;
 		}
+
+		if (!(string.IsNullOrEmpty (timesMedium))) {
+			dataTimeInSecondsMedium.text =  timesMedium;
+		} else {
+			dataTimeInSecondsMedium.text = string.Empty;
+		}
+
+		if (!(string.IsNullOrEmpty (timesHigh))) {
+			dataTimeInSecondsHigh.text = timesHigh;
+		} else {
+			dataTimeInSecondsHigh.text = string.Empty;
+		}
+		dataLow.text = "L: "+low.ToString ();
+		dataMedium.text = "M: "+medium.ToString ();
+		dataHigh.text = "H: "+high.ToString ();
+
 
 	}
 		
@@ -80,7 +113,7 @@ public class Graph : MonoBehaviour {
 
 	public void DestroyTimeList(ArrayList arr){
 		for (int i = 0; i < arr.Count; i++) {
-			timeOfSpike.RemoveAt (i);
+			arr.RemoveAt (i);
 		}
 	}
 
@@ -99,20 +132,13 @@ public class Graph : MonoBehaviour {
 		string newString = sb.ToString().TrimEnd(MyChar);
 		return newString;
 	}
-
-	public float Truncate(float value, int digits)
-	{
-		double mult = Math.Pow(10.0, digits);
-		double result = Math.Truncate( mult * value ) / mult;
-		return (float) result;
-	}
+		
 
 	public void  VisGraph(int sliderData){
 
    //     Debug.Log("In VisGraph_SliderData is "+ sliderData);
 
-		for (int i = sliderData; i < sliderData +10; i++) {
-
+		for (int i = sliderData; i < sliderData +maxRange; i++) {
 
 			xval = float.Parse(data[i]["xval"].ToString());
 			yval = float.Parse(data[i]["yval"].ToString());
@@ -150,8 +176,10 @@ public class Graph : MonoBehaviour {
 			if (nspk == 1) {
 				
 				noOfSpikes++;
+				lowSpikes++;
 				timeSec = ntime / 10000;
-				timeOfSpike.Add (timeSec);
+				//timeOfSpike.Add (timeSec);
+				timeOfSpikeLow.Add (timeSec);
 				nspikeVal = nspk * nspikeCondenseValue;
 				Vector3 vect3intensity = new Vector3(xval , nspikeVal, yval);
 				Instantiate (pointOneSpkiePrefab, vect3intensity, Quaternion.identity);
@@ -159,8 +187,11 @@ public class Graph : MonoBehaviour {
 			if (nspk == 2) {
 
 				noOfSpikes+=2;
-				timeOfSpike.Add (ntime / 10000);
-				timeOfSpike.Add (ntime2 / 10000);
+				mediumSpikes++;
+		//		timeOfSpike.Add (ntime / 10000);
+		//		timeOfSpike.Add (ntime2 / 10000);
+				timeOfSpikeMedium.Add(((ntime+ntime2)/2)/10000);
+		//		timeOfSpikeMedium.Add (ntime2 / 10000);
 				nspikeVal = nspk * nspikeCondenseValue;
 				Vector3 vect3intensity = new Vector3(xval , nspikeVal, yval);
 				Instantiate (pointTwoSpkiePrefab, vect3intensity, Quaternion.identity);
@@ -169,9 +200,10 @@ public class Graph : MonoBehaviour {
 			if (nspk == 3) {
 
 				noOfSpikes+=3;
-				timeOfSpike.Add (ntime / 10000);
-				timeOfSpike.Add (ntime2 / 10000);
-				timeOfSpike.Add (ntime3 / 10000);
+				highSpikes++;
+				timeOfSpikeHigh.Add (((ntime+ntime2+ntime3)/3)/10000);
+		//		timeOfSpikeHigh.Add (ntime2 / 10000);
+		//		timeOfSpikeHigh.Add (ntime3 / 10000);
 				nspikeVal = nspk * nspikeCondenseValue;
 				Vector3 vect3intensity = new Vector3(xval , nspikeVal, yval);
 				Instantiate (pointThreeSpkiePrefab, vect3intensity, Quaternion.identity);
@@ -179,10 +211,18 @@ public class Graph : MonoBehaviour {
 
 		}
 
-		string times = CreateStringOfTime (timeOfSpike);
-		showRange(sliderData, noOfSpikes,times);
-		DestroyTimeList (timeOfSpike);
+		string timesLow = CreateStringOfTime (timeOfSpikeLow);
+		string timesMedium = CreateStringOfTime (timeOfSpikeMedium);
+		string timesHigh = CreateStringOfTime (timeOfSpikeHigh);
+
+		showRange(sliderData, noOfSpikes,lowSpikes,highSpikes,mediumSpikes,timesLow,timesMedium,timesHigh);
+		DestroyTimeList (timeOfSpikeLow);
+		DestroyTimeList (timeOfSpikeMedium);
+		DestroyTimeList (timeOfSpikeHigh);
 		noOfSpikes = 0;
+		lowSpikes = 0;
+		mediumSpikes = 0;
+		highSpikes = 0;
 	}
 	
 
